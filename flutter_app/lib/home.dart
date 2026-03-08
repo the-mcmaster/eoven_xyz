d9d4bfa4b16e1aaba672d48c123b7030ef603c25
Eric Ovenden <eric2ovenden@gmail.com>
2026-03-08 03:40:05 -0500

    Pretty solid build of the website

--- flutter_app/lib/home.dart
+++ flutter_app/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,32 +10,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  EdgeInsets _paragraphPad(double width) {
+    return EdgeInsets.fromLTRB(width, 50, width, 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     ColorScheme colors = Theme.of(context).colorScheme;
-    const double sideMargins = 16;
+    Size screenSize = MediaQuery.sizeOf(context);
+
+    const double desiredHomeWidth = 900;
+    const double desiredParagraphPadding = 50;
+    double minHomeWidth = (screenSize.width > desiredHomeWidth)
+        ? screenSize.width
+        : desiredHomeWidth;
+    double paragraphPadding = (screenSize.width - desiredHomeWidth >
+            2 * desiredParagraphPadding)
+        ? (screenSize.width - desiredHomeWidth - 2 * desiredParagraphPadding) /
+            2
+        : (screenSize.width - desiredHomeWidth > 0)
+            ? screenSize.width - desiredHomeWidth
+            : 0;
+
+    Widget paragraphEntry(String data) {
+      return textEntry(
+        colors,
+        data,
+        withPadding: _paragraphPad(paragraphPadding),
+      );
+    }
 
     return Container(
       color: colors.primary,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(sideMargins, 10, sideMargins, 10),
-        child: Column(
-          children: [
-            textEntry(colors, "Eric Ovenden",
-                fontSize: 40, fontWeight: FontWeight.bold),
-            textEntry(
-              colors,
-              "A page dedicated to displaying interests, hobbies, and skills.",
-              withPadding: EdgeInsets.fromLTRB(200, 50, 200, 8),
-            ),
-            textEntry(
-              colors,
-              "This website is still under construction",
-              fontStyle: FontStyle.italic,
-              withPadding: EdgeInsets.fromLTRB(200, 8, 200, 8),
-            ),
-          ],
-        ),
+      width: minHomeWidth,
+      child: Column(
+        children: [
+          textEntry(
+            colors,
+            "Eric Ovenden",
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+          paragraphEntry(
+            "A page dedicated to displaying interests, hobbies, and skills. This website showcases my blog posts, software projects, and some external links.",
+          ),
+          paragraphEntry(
+            "The button icons at the top of the screen can navigate to my other pages.",
+          ),
+          textEntry(
+            colors,
+            "This website is still very much under construction.",
+            fontStyle: FontStyle.italic,
+            withPadding: _paragraphPad(paragraphPadding),
+          ),
+        ],
       ),
     );
   }
