d90508a1e1dee3a4504415e255c3643517491a79
Eric Ovenden <eric2ovenden@gmail.com>
2026-03-07 22:15:48 -0600

    Built a better build script

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3860648989" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2263658416" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
